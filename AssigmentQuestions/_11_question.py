47ba9d46a98160c69ca931478f5dec42385a6e75
aydin <muhammedaydin9214@gmail.com>
2020-07-12 19:17:12 +0200

    assign

--- AssigmentQuestions/_11_question.py
+++ AssigmentQuestions/_11_question.py
@@ -1,13 +1,16 @@
-def prime_number(x):
+def prime_number():
     """
      a function that takes a number from the user and prints the result to check if it is a prime number.
      prime number: a number that is divisible only by itself and 1
     """
-    for i in range(2, x):
-        if x % i:
-            return f"{x} is a prime number"
-        else:
-            return f"{x} is not a prime number"
+    n = int(input("enter a number: "))
+    count = 0
+    for i in range(1, n + 1):
+        if not (n % i): count += 1
+    if n == 0 or n == 1 or count >= 3:
+        print(n, "is not a prime number")
+    else:
+        print(n, "is a prime number")
 
 
-print(prime_number(10))
+prime_number()
