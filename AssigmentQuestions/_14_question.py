9d4825fa85d88ecf08d61118bc9a23a72e98c17a
aydin <muhammedaydin9214@gmail.com>
2020-07-31 18:28:59 +0200

    assing

---
+++ AssigmentQuestions/_14_question.py
@@ -0,0 +1,21 @@
+def fizz_buzz():
+    """
+    Print numbers from 1 to 100 inclusively following these instructions:
+    if a number is multiple of 3, print "Fizz" instead of this number,
+    if a number is multiple of 5, print "Buzz" instead of this number,
+    for numbers that are multiples of both 3 and 5, print "FizzBuzz",
+    print the rest of the numbers unchanged.
+    Output each value on a separate line.
+    """
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print(i, "=>", "FizzBuzz")
+        elif i % 5 == 0:
+            print(i, "=>", "Buzz")
+        elif i % 3 == 0:
+            print(i, "=>", "Fizz")
+        else:
+            print(i)
+
+
+fizz_buzz()
